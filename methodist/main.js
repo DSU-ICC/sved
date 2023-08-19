84047c1354facc289e187b5e9aff5e297fd975c4
Akhmed8251 <88936648+Akhmed8251@users.noreply.github.com>
2023-08-19 15:33:48 +0300

    изменил название РПД на РПД и ФОС в админке

--- methodist/main.js
+++ methodist/main.js
@@ -1345,13 +1345,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (String(el.profile.linkToRPD).toString() != "NULL" && el.profile.linkToRPD != null) {
                     res += `
                         <td itemprop="educationRpd">
-                            <a href="${el.profile.linkToRPD}">РПД</a>
+                            <a href="${el.profile.linkToRPD}">РПД и ФОС</a>
                         </td>
                     `
                 } else {
                     res += `
                         <td itemprop="educationRpd">
-                            <a href="${URL}/sved/methodist/eor.html?profileId=${el.profile.id}">РПД</a>
+                            <a href="${URL}/sved/methodist/eor.html?profileId=${el.profile.id}">РПД и ФОС</a>
                         </td>
                     `
                 }
