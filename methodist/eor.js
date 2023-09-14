2edc4930e2290548aa062d5420e22f91b2dcd540
Akhmed8251 <bratya_250@mail.ru>
2023-09-14 10:31:34 +0300

    рефакторинг

--- methodist/eor.js
+++ methodist/eor.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     var path = window.location.pathname; var host = window.location.hostname;
     document.getElementById("specialVersion").href = "https://finevision.ru/?hostname=" + host + "&path=" + path
-    //const URL = "https://oop.dgu.ru"
-    const URL = "https://localhost:44370"
+    const URL = "https://oop.dgu.ru"
+    //const URL = "https://localhost:44370"
     let logoutBtn = document.querySelector(".header .action__btn")
     let closeModalBtns = document.querySelectorAll(".popup__close")
     let popupUploadFileRpd = document.querySelector("#popup-createRpd")
